3228a60874da8ad01503816d0a13eb1599ab0e1b
yayawu <sandy50422@gmail.com>
2018-11-09 22:30:44 +0800

    繼續建立

---
+++ aspdotnetmvc/aspdotnetmvc/Controllers/TwoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+/// <summary>
+/// main test
+/// </summary>
+namespace aspdotnetmvc.Controllers
+{
+    public class TwoController : Controller
+    {
+        // GET: Two
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ aspdotnetmvc/aspdotnetmvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="aspdotnetmvc.MvcApplication" Language="C#" %>
